fac1ba332bd83d2fb4c6547d8eb558cde00cbae8
Sam Gleske <sam.mxracer@gmail.com>
2022-03-24 22:35:05 -0400

    add support for local dev again

--- start-jekyll.sh
+++ start-jekyll.sh
@@ -4,4 +4,7 @@ docker run \
   --rm -w "$PWD" -e HOME="$PWD" -v "$PWD:$PWD" \
   --user "$(id -u):$(id -g)" -p 4000:4000 \
   ruby:2.6 \
-  /bin/bash -lc 'bundle install --deployment;bundle exec jekyll serve -H 0.0.0.0'
+  /bin/bash -lc \
+    'bundle install --deployment;
+     bundle exec \
+       jekyll serve --config _config.yml,_config_local.yml -H 0.0.0.0'
